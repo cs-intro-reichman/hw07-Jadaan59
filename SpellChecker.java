19e5a0812675d709caa8d330f0c5d569092da561
Gidon Abbas <89697081+Jadaan59@users.noreply.github.com>
2024-02-04 18:27:13 +0200

    Update SpellChecker.java
    with commentation.

--- SpellChecker.java
+++ SpellChecker.java
@@ -10,10 +10,20 @@ public class SpellChecker {
 		System.out.println(correction);
 	}
 
+	// tail(a), is the word a, excluding the first character. //
 	public static String tail(String str) {
 		return str.substring(1);
 	}
 
+	/*
+Is a string metric for measuring the difference between two strings.
+	The distance between two words is the minimum number of single-character edits
+	 (insertions, deletions or substitutions) needed to transform one word into the other word.
+	 levenshtein(“hello”, “hell”); // 1 (one deletion)
+	 levenshtein(“hello”, “hell0”); // 1 (one substitution)
+ 	 levenshtein(“love”, “i”); // 4 (one substitution, three deletions)
+	 levenshtein(“concensus”, “consensus”); // 1
+	 */
 	public static int levenshtein(String word1, String word2) {
 		if (word2.length() == 0){
 			return word1.length();
@@ -27,6 +37,7 @@ public class SpellChecker {
 								 levenshtein(tail(word1),tail(word2)))));
 	}
 
+	/*read a local file of words, and store them in an array. */
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
@@ -39,6 +50,10 @@ public class SpellChecker {
 		return dictionary;
 	}
 
+	/*
+	 * receives a word, a threshold value for distance, and a dictionary as inputs.
+	 * It returns the word from the dictionary that most closely resembles the given word.
+	 */
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		String bestMatch = word;
 		int bestDistance = threshold;
