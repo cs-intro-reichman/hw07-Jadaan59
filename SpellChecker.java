d02e58a9cebbde15c7b356056a778a9d248178b0
Gidon Abbas <89697081+Jadaan59@users.noreply.github.com>
2024-02-04 16:32:21 +0200

    Update SpellChecker.java
    first try

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,11 +11,21 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		return str.substring(1);
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		if (word2.length() == 0){
+			return word1.length();
+		}if (word1.length() == 0){
+			return word2.length();
+		}if (word1.charAt(0) == word2.charAt(0)){
+			return levenshtein(tail(word1), tail(word2));
+		}else {
+			return (1 +(Math.min(levenshtein(tail(word1) , word2),
+					    Math.min(levenshtein(word1, tail(word2)),
+							     levenshtein(tail(word1), tail(word2))))));
+		}
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -23,13 +33,21 @@ public class SpellChecker {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i = 0; i < 3000; i++){
+			dictionary[i] = in.readLine();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		for (int i = 0; i < dictionary.length; i++){
+			 if (levenshtein( word ,dictionary[i]) < threshold){
+				word = dictionary[i];
+				threshold = levenshtein( word ,dictionary[i]);
+			 }
+		}
+		return word;
 	}
 
 }
